3f5a8010c88bfcdf991063a1f26e86dc1cca07b7
shuangxiangkan <47422069+shuangxiangkan@users.noreply.github.com>
2025-10-24 12:58:38 +1100

    add libzip lib

--- tools/driver/main.py
+++ tools/driver/main.py
@@ -160,7 +160,8 @@ if __name__ == "__main__":
     # config_path = "/home/kansx/SVF-Tools/Slicer/tools/driver/configs/hdf5/hdf5.yaml"
     # config_path = "/home/kansx/SVF-Tools/Slicer/tools/driver/configs/cgltf/cgltf.yaml"
     # config_path = "/home/kansx/SVF-Tools/Slicer/tools/driver/configs/StormLib/StormLib.yaml"
-    config_path = "/home/kansx/SVF-Tools/Slicer/tools/driver/configs/libtiff/libtiff.yaml"
+    # config_path = "/home/kansx/SVF-Tools/Slicer/tools/driver/configs/libtiff/libtiff.yaml"
+    config_path = "/home/kansx/SVF-Tools/Slicer/tools/driver/configs/libzip/libzip.yaml"
     
     library_type = "static"  # "static", "shared"
     
